300b654f5fd0e3ef0b85318105c316334c9032e2
Felipe Muhamed <muhamed@cpqd.com.br>
2018-05-25 13:39:53 -0300

    Added socket io module and method connection

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socket = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socket(server);
+
+io.on('connection', (socket) =>{
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    })
+});
+
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
-    console.log('Server is up on port ${port}');
+server.listen(port, () => {
+    console.log(`Server is up on ${port}`);
 })
